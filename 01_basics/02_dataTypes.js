29af9ebb5baf5e937c01c85ff50108e3f6d7ea49
Himanshu9112 <8009himanshuyadav@gmail.com>
2024-07-13 17:25:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat as all JS as Newer Version
+// alertI(3+3) // we are using nodejs, not browser
+console.log (3+
+    3);          //code readablity should be high
+
+console.log("himanshu")
+
+let name = "him"
+let age = 18
+let isLoggedIn = false
+
+
+//number =>2 to power 53
+//biint=> when number is too large then we can use 
+//string => ""
+//boolean => true/false
+//null =>standalone Value means its not undefined its voide 
+//undefined => value not defined Yet
+//symbol => for defining unique things 
+
+
+
+//object
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
